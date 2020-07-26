454125b5e77841e0eb6ade7648c15b71ccd6b05e
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2020-07-26 11:58:50 -0400

    calendar

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ _____________________________  |  _______________________________
 
 <b> SU  MO  TU  WE  TH  FR  SA</b>    |  |OOOOOOOOOO <b>11</b>              |
 
- 28  29  30   1   2   3   4    |  |OOOOOOOOOOO <b>12</b>                                                 |
+ 28  29  30   1   2   3   4    |  |OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO <b>58</b>   |
 
   5   6   7   8   9  10  11    |  \0_________10________20________30________40________50________60/
 
@@ -19,6 +19,30 @@ _____________________________  |  _______________________________
  19  20  21  22  23  24  25  
 
 <b>[26]</b> 27  28  29  30  31   1  
+
+------------------------------------------------------
+
+<b>Book List</b> (Some of my favorite books read in the last 2-3 years)
+
+<b><a href="https://www.goodreads.com/book/show/37542581-the-spy-and-the-traitor">The Spy And The Traitor</a></b>: <i>Story of Oleg Gordievsky, a real soviet era spy turned double agent for MI6.</i>
+
+<b><a href="https://www.goodreads.com/book/show/12617.Manufacturing_Consent">Manufacturing Consent</a></b>: <i>Classic Chomskey novel relating the politics and media. Regardless of ones opinion, it presents a lot of useful context and example relating to how media will shape the view of political events.</i>
+
+<b><a href="https://www.goodreads.com/book/show/8049273-the-elegant-universe">The Elegant Universe</a></b>: <i>Brian Greene tries his hand at explaining string theory to the layman and he succeeds. Much of the material is up for debate, however it offers a lot of interesting knowledge relating to both the universe and the mathematics behind multi-demensional universes.</i>
+
+<b><a href="https://www.goodreads.com/book/show/39736150-the-little-typer">The Little Typer</a></b>: <i>Great introduction to dependent types in an easy to follow book. Also great demonstration of the power of the Racket programming language</i>
+
+<b><a href="https://www.goodreads.com/book/show/11468377-thinking-fast-and-slow">Thinking Fast and Slow</a></b>: <i>Deep dive into how we think and the different systems surrounding thinking.</i>
+
+<b><a href="https://www.goodreads.com/book/show/17994.The_Code_Book">The Code Book</a></b>: <i>Cool history of the evolution of criptography through the ages and how different types of codes work and are broken. The part on the enigma machine is especially interesting.</i>
+
+<b><a href="https://www.goodreads.com/book/show/44525305-the-ride-of-a-lifetime">The Ride Of A Lifetime</a></b>: <i>Autobiography from former Disney CEO Robert Iger. Through written before his retirement, it offers an insight into how to rise in complex organization using strong relationships and motivation.</i>
+
+<b><a href="https://www.goodreads.com/book/show/24724602-flash-boys">Flash Boys</a></b>: <i>Story of how high frequency trading started and evolved along with the consequences of it.</i>
+
+<b><a href="https://www.goodreads.com/book/show/703102.Compilers">Compilers - Principles, Techniques & Tools</a></b>: <i>Interesting read. Great as a reference when working on compilers. Even when not in the business of implementing compilers, offers a lot of details on generally useful algorithms (especially relating to parsing techniques).</i>
+
+<b><a href="https://www.goodreads.com/book/show/142239.Understanding_Exposure">Understanding Exposure</a></b>: <i>Must read for anyone interested in photography. Covers a wide range of techniques about how to shoot just about anything.</i>
 
 
 
--- generate.py
+++ generate.py
@@ -90,6 +90,23 @@ def join_blocks(left: str, right: str, delimiter="  |  "):
     return block
 
 
+def renderBook(title: str, link: str, description) -> str:
+    return f"<b><a href=\"{link}\">{title}</a></b>: <i>" + description + "</i>\n\n"
+
+
+def book_list():
+    return "------------------------------------------------------\n\n<b>Book List</b> (Some of my favorite books read in the last 2-3 years)\n\n" + \
+        renderBook("The Spy And The Traitor", "https://www.goodreads.com/book/show/37542581-the-spy-and-the-traitor", "Story of Oleg Gordievsky, a real soviet era spy turned double agent for MI6.") + \
+        renderBook("Manufacturing Consent", "https://www.goodreads.com/book/show/12617.Manufacturing_Consent", "Classic Chomskey novel relating the politics and media. Regardless of ones opinion, it presents a lot of useful context and example relating to how media will shape the view of political events.") + \
+        renderBook("The Elegant Universe", "https://www.goodreads.com/book/show/8049273-the-elegant-universe", "Brian Greene tries his hand at explaining string theory to the layman and he succeeds. Much of the material is up for debate, however it offers a lot of interesting knowledge relating to both the universe and the mathematics behind multi-demensional universes.") + \
+        renderBook("The Little Typer", "https://www.goodreads.com/book/show/39736150-the-little-typer", "Great introduction to dependent types in an easy to follow book. Also great demonstration of the power of the Racket programming language") + \
+        renderBook("Thinking Fast and Slow", "https://www.goodreads.com/book/show/11468377-thinking-fast-and-slow", "Deep dive into how we think and the different systems surrounding thinking.") + \
+        renderBook("The Code Book", "https://www.goodreads.com/book/show/17994.The_Code_Book", "Cool history of the evolution of criptography through the ages and how different types of codes work and are broken. The part on the enigma machine is especially interesting.") + \
+        renderBook("The Ride Of A Lifetime", "https://www.goodreads.com/book/show/44525305-the-ride-of-a-lifetime", "Autobiography from former Disney CEO Robert Iger. Through written before his retirement, it offers an insight into how to rise in complex organization using strong relationships and motivation.") + \
+        renderBook("Flash Boys", "https://www.goodreads.com/book/show/24724602-flash-boys", "Story of how high frequency trading started and evolved along with the consequences of it.") + \
+        renderBook("Compilers - Principles, Techniques & Tools", "https://www.goodreads.com/book/show/703102.Compilers", "Interesting read. Great as a reference when working on compilers. Even when not in the business of implementing compilers, offers a lot of details on generally useful algorithms (especially relating to parsing techniques).") + \
+        renderBook("Understanding Exposure", "https://www.goodreads.com/book/show/142239.Understanding_Exposure", "Must read for anyone interested in photography. Covers a wide range of techniques about how to shoot just about anything.")
+
 def wrap_in_ticks(content: str):
     new_content = ""
     for line in content.split("\n\n"):
@@ -109,7 +126,8 @@ if __name__ == '__main__':
                                                                                                  bottom_bar=False)
                                                                          + generate_progress_bar(datetime.now().minute,
                                                                                                  top_bar=False)
-                                                                         )
+                                                                         ) +
+                                                                        book_list()
                                                              )
     print(content)
 
